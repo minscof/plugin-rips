57af6cfd9117be4f45901be6eec1aa53b00338df
minscof <minscof@hotmail.com>
2018-03-07 00:06:33 +0100

    add neuron network to analyze rssi values and find location

--- resources/rips_listen_antenna.py
+++ resources/rips_listen_antenna.py
@@ -2,11 +2,12 @@ import os
 import sys
 import traceback
 import socket
-from threading import Thread, Lock
+from threading import Thread, Lock, Timer
 import json
 import time
 from collections import deque
 import logging
+from fann2 import libfann
 
 
 debug = True
@@ -14,8 +15,8 @@ FORMAT = '%(asctime)s - %(message)s'
 CAPTURE_FILE = "captureRssi.txt"
 MAX_ITEM = 2000
 queue = None
-mode = 'Learn'
-#mode = 'Analyze'
+#mode = 'Learn'
+mode = 'Analyze'
 mutex = Lock()
 status = "scan"
 
@@ -28,6 +29,169 @@ else :
     logLevel=logging.CRITICAL    
     logging.basicConfig(format=FORMAT,level=logLevel)
 
+class Neuron:
+    def __init__(self):
+        self.ann = libfann.neural_net()
+        self.ann.create_from_file("training_result")
+        
+    def run(self,sample):
+        result = self.ann.run(sample)
+        print (result)
+        return result
+
+ann = Neuron();
+
+class Antennas:
+    ANTENNA1 = 'raspberrypi'
+    ANTENNA2 = 'Jeedom'
+    ANTENNA3 = 'cuisine'
+    ANTENNAS = [ANTENNA1,ANTENNA2,ANTENNA3]
+    
+      
+class Rooms:
+    SALON = 'Salon'
+    ENTREE = 'Entrée'
+    CHAMBRE = 'Chambre'
+    UNKNOWN = 'Unknown'
+    MISSING = 'Missing'
+    LOCATIONS = [SALON,ENTREE,CHAMBRE]
+    
+      
+class Tags:
+    tags_dict = {}
+    def __init__(self,mac):
+        self.mac = mac
+        self.position = Rooms.UNKNOWN
+        self.rssiValues = []
+        Tags.tags_dict[mac]=self
+    
+    def get_mac(self):
+        return self.mac
+    
+    def set_room(self,room):
+        self.position = room
+    
+    def get_room(self):
+        return self.position
+    
+    def add_rssi(self,antenna,timestamp,rssi):
+        global mutex
+        mutex.acquire()
+        self.rssiValues.append([antenna, timestamp,rssi])
+        mutex.release()
+        
+    def remove_rssi(self,antenna,timestamp,rssi):
+        global mutex
+        mutex.acquire()
+        self.rssiValues.remove([antenna, timestamp,rssi])
+        mutex.release()
+    
+    def remove_first_rssi(self):
+        global mutex
+        mutex.acquire()
+        self.rssiValues.pop(0)
+        mutex.release()
+    
+    def list_rssi(self):   
+        for tab in self.rssiValues :
+            print ('value = ',tab)
+    
+    def get_rssi(self):   
+        return self.rssiValues
+    
+    def first_rssi(self):
+        if len(self.rssiValues) > 0 :   
+            return self.rssiValues[0]
+        else :
+            return []
+    
+    def last_rssi(self):
+        if len(self.rssiValues) > 0 :   
+            return self.rssiValues[-1]
+        else :
+            return []
+    
+    def count_rssi(self):
+        return len(self.rssiValues)
+
+
+class AnalyzeRips:
+    __deltat = 5
+    __missingRssi = -110
+        
+    @staticmethod
+    def check_change():
+        global ann
+        ts = int(time.time())
+        print ('Analyze Position @ ',ts)
+        for mac in Tags.tags_dict:
+            tag = Tags.tags_dict[mac]
+            print ("analyse tag %s in room %s",mac,tag.get_room())
+            
+            #mac = 'f0:46:00:0b:8b:01'
+            first = tag.first_rssi()
+            if first == []:
+                print("no rssi for this Tag")
+                break
+            deltat = ts-first[1]
+            sample = [AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi,AnalyzeRips.__missingRssi]
+            if (deltat > 4*AnalyzeRips.__deltat) or ((deltat > 3*AnalyzeRips.__deltat) and (tag.count_rssi() > 3)) :
+                values = tag.get_rssi()
+                for value in values :
+                    antenna,timestamp,rssi = value
+                    deltat = ts-timestamp
+                    index = Antennas.ANTENNAS.index(antenna) *3
+                    if deltat > 2*AnalyzeRips.__deltat :
+                        sample[index+2] = rssi
+                    elif deltat > AnalyzeRips.__deltat :
+                        sample[index+1] = rssi
+                    else :
+                        sample[index] = rssi
+                    
+                    '''
+                    for i in [index+2,index+1,index]:
+                        if sample[i] == AnalyzeRips.__missingRssi:
+                            sample[i] = rssi
+                            break
+                        else :
+                            if i == index+2:
+                               sample[i] = rssi 
+                    '''
+                if sample[index+1] == AnalyzeRips.__missingRssi and sample[index] != AnalyzeRips.__missingRssi and sample[index+2] != AnalyzeRips.__missingRssi :
+                    print("** correction add missing value @ t0+1")
+                    sample[index+1] = int((sample[index] + sample[index+2]) / 2)
+                    
+                tag.remove_first_rssi()
+                print("sample =",sample)
+                positions = ann.run(sample)
+                score = max(positions)
+                if score > 0.9:
+                    room = Rooms.LOCATIONS[positions.index(score)]
+                    if tag.get_room() != room:
+                        tag.set_room(room)
+                        print (" ======= new location for %s is %s",tag.get_mac(),room)
+                    elif max(sample) == AnalyzeRips.__missingRssi:
+                        tag.set_room(Rooms.MISSING)
+                    else :
+                        tag.set_room(Rooms.UNKNOWN)
+                
+                while True:
+                    first = tag.first_rssi()
+                    if first == [] :
+                        break
+                    deltat = ts-first[1]
+                    if deltat > 3*AnalyzeRips.__deltat :
+                        print ("remove old values",first)
+                        tag.remove_first_rssi()
+                    else:
+                        break
+                print("exit check change")
+                        
+                    
+                         
+                
+    
+
 
 def start_rips_server():
     host = "127.0.0.1"
@@ -35,7 +199,7 @@ def start_rips_server():
 
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
-    print("Socket created port:",port)
+    print("Socket created host %s port %i:",host,port)
 
     try:
         soc.bind((host, port))
@@ -122,47 +286,70 @@ def write_buffer(queue):
     mutex.release() 
     file.close() 
         
-def analyze_position(queue):
-    mutex.acquire()
-    
-    mutex.release()
-    return
+
 
 def process_input(input_str):
     global queue, MAX_ITEM, mode, mutex
+    
     #print("Processing the input received from client",input_str)
     ts = int(time.time())
     if "--" in input_str:
         return input_str
-    data = json.loads(input_str)
+    #data = json.loads(input_str)
+    antenna,mac,timestamp,rssi = json.loads(input_str)
+    logging.debug('Tag %s seen @ %i - rssi= %i from %s',mac,timestamp,rssi,antenna)
     #logging.debug('Tag %s seen @ %i - rssi= %i from %s',data[1],data[2],data[3],data[0])
-    if ts != data[2]:
+    if ts != timestamp:
         #print ("timestampSRV=",ts)
         pass
-    mutex.acquire()
-    queue.append(data)
-    mutex.release()
+    
     if mode == "Analyze" :
-        analyze_position(queue)
+        if mac not in Tags.tags_dict:
+            tag = Tags(mac)
+        else :
+            tag = Tags.tags_dict[mac]
+        tag.add_rssi(antenna, timestamp, rssi)
+    
+    if mode == 'Learn':
+        mutex.acquire()
+        queue.append([antenna,tag,timestamp,rssi])
+        mutex.release()
+        if len(queue) > MAX_ITEM:
+            write_buffer(queue)
+        
     #logging.debug('len queue',len(queue))
     #print ("len queue %i max %i",len(queue),MAX_ITEM)
-    if len(queue) > MAX_ITEM:
-        if mode == 'Learn':
-            write_buffer(queue)
-        else :
-            mutex.acquire()
-            queue.popleft()
-            mutex.release()
-            
+        
     return "Decode= " + str(input_str).upper()
 
 
-def file_len(fname):
-    i=-1
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+
+class MyTimer: 
+    def __init__(self, tempo, target, args= [], kwargs={}): 
+        self._target = target 
+        self._args = args 
+        self._kwargs = kwargs 
+        self._tempo = tempo 
+  
+    def _run(self): 
+        self._timer = Timer(self._tempo, self._run) 
+        self._timer.start() 
+        self._target(*self._args, **self._kwargs) 
+  
+    def start(self): 
+        self._timer = Timer(self._tempo, self._run) 
+        self._timer.start() 
+  
+    def stop(self): 
+        self._timer.cancel() 
+
+
+def analyze_position(queue):
+    #print('analyse change')
+    AnalyzeRips.check_change()
+    
+
+
 
 class Bcolors:
     HEADER = '\033[95m'
@@ -185,11 +372,13 @@ def init_rips_listen_server():
     except:
         print(Bcolors.FAIL+"Thread did not start."+Bcolors.ENDC)
         traceback.print_exc()
-    #start_rips_server()
     
 
 if __name__ == "__main__":
     init_rips_listen_server()
+    
+    periodic = MyTimer(5.0, analyze_position, [queue])
+    periodic.start()
     
     time.sleep(2)
     print(Bcolors.HEADER+"Enter 'quit' to exit"+Bcolors.ENDC)
@@ -209,6 +398,13 @@ if __name__ == "__main__":
         write_buffer(queue)
         count = file_len(CAPTURE_FILE)
         print (Bcolors.BOLD+"recorded count =",count)
+    
+    periodic.stop()
+    for mac in Tags.tags_dict :
+        print ("tag mac %s",mac)
+        tag = Tags.tags_dict[mac]
+        print ("values =",tag)
+        tag.list_rssi()
     
     
     print(Bcolors.OKGREEN+"exit listening"+Bcolors.ENDC)
